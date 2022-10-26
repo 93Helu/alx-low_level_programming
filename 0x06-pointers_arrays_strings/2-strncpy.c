75d508d85783acc1de199f86b3a48e411ae4dfb1
93Helu <zelekeh93@gmail.com>
2022-10-26 08:00:13 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcat - function that copies a string
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: third parameter
+ * Return: the pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n);
+{
+	strncpy(dest, src. n);
+	return (dest);
+}

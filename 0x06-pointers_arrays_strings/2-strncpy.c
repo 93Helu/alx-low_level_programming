3834d23fe8f44959714e8451042381777380aede
93Helu <zelekeh93@gmail.com>
2022-10-26 15:09:03 -0700

    commit task 1 to 12

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,20 @@
 #include "main.h"
-
 /**
- * _strcat - function that copies a string
- * @dest: first parameter
- * @src: second parameter
- * @n: third parameter
- * Return: the pointer to dest
+ * _strncpy - copies a string
+ * @dest: destination pointer.
+ * @src: source pointer.
+ * @n: amount of bytes/indexes from src.
+ * Return: the pointer to dest.
  */
-char *_strncpy(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n)
 {
-	strncpy(dest, src. n);
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }

8b813da9ba380a766ad7fceeaacdda2fb9217d8c
Helen <114483343+93Helu@users.noreply.github.com>
2022-10-26 08:20:07 -0700

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-char *_strcat(char *dest, char *src);
+int _putchar(char);
+char *_strncat(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);

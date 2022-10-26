88f5ee1e5465e184141fef9658c81184d4cd460d
93Helu <zelekeh93@gmail.com>
2022-10-26 09:26:35 -0700

    commit strcat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,6 +9,20 @@
  */
 char *_strncat(char *dest, char *src, int n);
 {
-	_strncat(dest, *src. n);
+int i = 0, j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+
+	while (j < n)
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+			break;
+		i++;
+		j++;
+	}
 	return (dest);
 }

d14c33d2940a3099d28d5860d7547ac2d411abe0
93Helu <zelekeh93@gmail.com>
2022-11-14 10:36:59 -0800

    dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,6 +6,7 @@
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
+ * Return: no return.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
